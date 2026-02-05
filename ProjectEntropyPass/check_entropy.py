79e23e6f09ff5e12988a3309e711e05469a65c1e
Alexander.Val <training.sysnet@gmail.com>
2026-02-05 14:49:06 +0300

    test:

--- ProjectEntropyPass/check_entropy.py
+++ ProjectEntropyPass/check_entropy.py
@@ -11,7 +11,7 @@ def entropy(password):
     if re.search(f"{[string.ascii_letters]}", password):
         res += len(string.ascii_letters)
     if re.search(f"{[string.digits]}", password):
-        res += len(string.ascii_letters)
+        res += len(string.digits)
     if re.search(f"{[string.punctuation]}", password):
         res += len(string.punctuation)
     entropy = log2(res**L)
--- ProjectEntropyPass/main.py
+++ ProjectEntropyPass/main.py
@@ -1,27 +1,6 @@
-from tkinter import *
-from check_entropy import entropy
-
-def GUI_tkinter():
-    def update_entropy():
-        password = e.get()
-        new_value = entropy(password)
-        lab2.config(text=f"Password Entropy: {round(new_value, 2)} bits")
-    root = Tk()
-    root.title("Password Entropy Check")
-    root.resizable(width=False, height=False)
-    root.geometry("300x250")
-    lab = Label(text="Enter the password", font=40)
-    lab.pack(pady=5)
-    e = Entry(root, show='*')
-    e.pack(pady=5)
-    lab2 = Label(text=f"Password Entropy: ")
-    lab2.pack(pady=5)
-    button = Button(root, text="CHECK", font=40, command=update_entropy)
-    button.pack(side=BOTTOM, pady=40)
+from gui import GUI_tkinter
 
 
-    
-    root.mainloop()
 
 
 
